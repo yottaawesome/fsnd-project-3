3810db4bbc6331a0d37b76fc3ef589232a9a6346
Vasilios Magriplis <etameson@gmail.com>
2019-06-03 22:22:12 +0930

    Remove test password from install.sql

--- src/scripts/install.sql
+++ src/scripts/install.sql
@@ -1,5 +1,5 @@
 create database bookshelf;
-create user bookshelfuser with encrypted password 'bookshelfuser';
+create user bookshelfuser with encrypted password '<password>';
 \connect bookshelf;
 grant all privileges on database bookshelf to bookshelfuser;
 grant all privileges on all tables in schema public to bookshelfuser;
